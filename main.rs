581098b84847742fd7b7550a1593a7b5bc4530f5
herenti <106109941+herenti@users.noreply.github.com>
2026-08-12 12:43:03 +0000

    Add files via upload

--- main.rs
+++ main.rs
@@ -9,11 +9,12 @@ use enable_ansi_support;
 
 /*
  * - INCOMPLETE.
+ * - TODO:
  * - dexterity to check for traps. mimics, door traps. fails or succeeds on chance.
  * - speech for better prices.
- * - set up location conditionals for events. advance user progress once that event is done. not until it is done.
- * - location data for cities can have multiple districts instead of everything in one.
- * - before location commands can be used the specific user flag must be lifted when an event needs to happen.
+ * - split sections into modules.
+ * - add interact and examine commands.
+ * - add npc struct.
 */
 
 fn save(user: &mut User) {
@@ -76,7 +77,6 @@ fn main() {
         }
         beginning = false;
     }
-
 }
 
 fn commands(input: &str, flag: &str) {
@@ -143,11 +143,20 @@ fn commands(input: &str, flag: &str) {
             }
             "travel" => {
                 if user.user_flags.contains(&"story_lock".to_string()) {
-                    println!("The {} command is not available right now.", "travel".green())
-
+                    println!("[{}] The {} command is not available right now.", "Game".blue().bold(), "travel".green())
                 } else {
-                    travel(args);
-                    events(false);
+                    let id = &travel_lookup(args);
+                    if id != "no" {
+                        if travel_check(id){
+                            travel(id);
+                            println!("[{}] traveled to {}.\r\n", "Game".blue().bold(), args);
+                            events(false);
+                        } else {
+                            println!("-\"[{}] I am not able to travel there right now...\"-", user.name.blue().bold());
+                        }
+                    } else {
+                        println!("[{}] That location is not available to travel to from here.", "Game".blue().bold());
+                    }
                 }
             }
             "quit" => {
@@ -159,14 +168,13 @@ fn commands(input: &str, flag: &str) {
                 clear;
                 println!("[{}] Incorrect command usage.", "ERROR".red().bold());
             }
-
         }
     }
 }
 
 fn events(flag: bool) {
     let mut user = load();
-    let mut to_progress = false;
+    let mut to_progress = true;
     let mut rsl = false;
     let mut ssl = false;
     for i in &user.event_flags {
@@ -174,76 +182,92 @@ fn events(flag: bool) {
             match user.progress { //if user.location == for location conditionals?
                 0 => {
                     println!("[{}] BANG!!!! THUMP. THUMP.", "Story".blue().bold());
-                    to_progress = true;
                 }
                 1 => {
                     println!("[{}] {} groaned and opened her eyes, laying in a confused stupor.", "Story".blue().bold(), user.name);
-                    to_progress = true;
                 }
                 2 => {
                     println!("[{}] -\"Must be the upstairs neighbors again...\"-", user.name.blue().bold());
-                    to_progress = true;
                 }
                 3 => {
                     println!("[{}] {} decided there was no use going back to sleep now. She wanted to give the landlord a peice of her mind, but knew it would be useless. Plus the rent was very cheap.", "story".blue().bold(), user.name);
-                    to_progress = true;
                 }
                 4 => {
                     println!("[{}] Getting up {} threw on the least filthy clothes she could find, and looked pitifully into the empty pantry. The mice had not even left a single crumb to eat, and the moldy cheese on the center table was certainly not edible.", "Story".blue().bold(), user.name);
-                    to_progress = true;
                 }
                 5 => {
                     println!("[{}] It was time to get more food with the remaining money from the last pay day.", "Story".blue().bold());
-                    to_progress = true;
                 }
                 6 => {
                     println!("[{}] {} grabbed the small purse off of the table. [20 {} added to the inventory]", "Story".blue().bold(), user.name, "coins".cyan());
                     inventory_update("coins", 10);
-                    to_progress = true;
                 }
                 7 => {
                     println!("[{}] Start by using the {} command to examine your area. This will show you people and things you can interact with using the {} command. It will also show you locations nearby that you can travel to from where you are using the {} command.", "Game".blue().bold(), "examine".green(), "interact".green(), "travel".green());
                     rsl = true;
-                    to_progress = true;
                 }
                 8 => {
                     if &user.location == "0002" {
                         println!("[{}] Blinking blearily in the intense mid day sun, {} almost bumps into someone passing by in the street.", "Story".blue().bold(), user.name);
                         ssl = true;
-                        to_progress = true;
-
                     } else if flag{
                         println!("[{}] -\"I need to go to the market for food...\"-", user.name.blue().bold());
+                        to_progress = false;
+                    } else{
                         to_progress = false;
                     }
                 }
                 9 =>{
                     println!("[{}] Watch it!", "Strange man".blue().bold());
-                    to_progress = true;
                 }
                 10 =>{
                     println!("[{}] Sorry!!! Sorry!", user.name.blue().bold());
-                    to_progress = true;
                 }
                 11 =>{
                     println!("[{}] The man grunts and walks away with a slouch. He is a full two heads taller than {}, with a rather rotund drinking belly, a balding head, and the strong smell of spice.", "Story".blue().bold(), user.name);
-                    to_progress = true;
                 }
                 12 =>{
                     println!("[{}] Spice is not an uncommon thing to smell in this part of the city. A mostly harmless substance but with addiction and withdrawals all the same. It can liven ones social skills and float you on a cloud for hours, but expect an irritable mood with a headache once it wears off...", user.name.blue().bold());
-                    to_progress = true;
                     rsl = true;
                 }
                 13 => {
                     if &user.location == "0003" {
                         println!("[{}] walking through the market, {} barely has time to react as a small figure darts out from behind a stall, deftly cuts the string of her purse, and runs off with it. [20 {} removed from the inventory].", "Story".blue().bold(), user.name, "coins".cyan());
                         ssl = true;
-                        to_progress = true;
-
                     } else if flag{
                         println!("[{}] -\"I need to go to the market for food...\"-", user.name.blue().bold());
                         to_progress = false;
+                    } else{
+                        to_progress = false;
                     }
+                }
+                14 => {
+                    println!("[{}] \"Stop!! Thief!!!!!\"", user.name.blue().bold());
+                }
+                15 => {
+                    println!("[{}] But the figure is already gone, weaving their way through the crowd. Some people look around at {}, but nobody saw it actually happen, and nobody really seems to care.", "Story".blue().bold(), user.name);
+                }
+                16 => {
+                    println!("[{}] -\"That was my only money for at least a week, no way is my boss going to pay me early.\"-", user.name.blue().bold());
+                }
+                17 => {
+                    println!("[{}] -\"I would cut off that thiefs hands myself if i could... What am i going to do?\"-", user.name.blue().bold());
+                }
+                18 => {
+                    println!("[{}] {} stands in the middle of the street completely hopeless for a few moments before catching a fragment of conversation:", "Story".blue().bold(), user.name);
+                }
+                19 => {
+                    println!("[{}] \"... Heard the guild has been making good money lately with it's job offers. Some trouble in the area with monsters ...\"", "Woman".blue().bold());
+                }
+                20 => {
+                    println!("[{}] -\"Thats right, the guild. I am going to starve if I don't get any money by tomorrow... Doing a guild job might be the only way to do it, but it could also get me killed...\"-", user.name.blue().bold());
+                }
+                21 => {
+                    println!("[{}] -\"I really dont have any other choice. Maybe i can find one that won't be too difficult?\"-", user.name.blue().bold());
+                }
+                22 => {
+                    println!("[{}]  -\"I need to go to Central Anshanli and then straight to the guild.\"-", user.name.blue().bold());
+                    rsl = true;
                 }
                 _ => {
 
@@ -255,21 +279,17 @@ fn events(flag: bool) {
     if to_progress {
         user.progress += 1;
     } else {
-
     }
     if rsl{
         let index = user.user_flags.iter().position(|x| *&x == "story_lock").unwrap();
         user.user_flags.remove(index);
     } else {
-
     }
     if ssl {
         user.user_flags.push("story_lock".to_string());
     } else {
-
     }
     save(&mut user);
-
 }
 
 fn inventory_update(item: &str, _amount: i32) {
@@ -312,25 +332,42 @@ fn inventory_update(item: &str, _amount: i32) {
     save(&mut user);
 }
 
-fn travel(place: &str) {
+fn travel(id: &str) {
+    let mut user = load();
+    user.location = id.to_string();
+    save(&mut user);
+}
+
+fn travel_lookup(place: &str) -> String {
     let mut user = load();
     let locations = Locations::new();
     let nearby = &locations.library[&user.location]["nearby"];
+    let mut id = "".to_string();
     for (key, value) in &locations.library {
         if nearby.contains(&key){
             if &locations.library[key]["name"][0] == place {
-                println!("[{}] Traveled to {}.", "game".blue().bold(), place);
-                user.location = key.to_string();
-                save(&mut user);
-                return;
+                return key.clone();
             } else {
-
             }
         } else {
-
         }
     }
-    println!("[{}] That location is not available to {} to from here.", "game".blue().bold(), "travel".green());
+    return "no".to_string();
+}
+
+fn travel_check(id: &str) -> bool {
+    let user = load();
+    if user.event_flags.contains(&"start".to_string()) {
+        if id == "0004" {
+            if user.progress < 22 {
+                return false;
+            } else {
+            }
+        } else {
+        }
+    } else {
+    }
+    return true;
 }
 
 struct User {
@@ -367,7 +404,7 @@ impl User {
     }
 }
 
-struct Objects {
+struct Objects { //rename Items
     items: HashMap<String, i32>, // value
     potions: HashMap<String, (String, i32, i32)>, // (type, value, statamount)
     weapons: HashMap<String, (i32, i32)>, // (value, damage)
@@ -382,7 +419,6 @@ impl Objects {
             weapons: HashMap::new(),
             scrolls: HashMap::new(),
         };
-
         objects.items.insert("coins".to_string(), 1);
         objects.potions.insert("minor health potion".to_string(), ("potion".to_string(), 25, 50));
         objects.weapons.insert("rusty dagger".to_string(), (10, 15));
@@ -401,34 +437,37 @@ impl Locations {
         let mut locations = Locations {
             library: HashMap::new(),
         };
-
-        let mut l0000 = HashMap::new();
-        l0000.insert("description".to_string(), vec!["A run down and kind of dirty single room apartment. There is a single Table in the center of the room with two chairs, a stove for cooking, and a few cupboards and countertops. There is not much else in this room besides a dresser for clothes and {} sitting on the table.".to_string()]);
-        l0000.insert("nearby".to_string(), vec!["0001".to_string()]);
-        l0000.insert("objects".to_string(), vec!["moldy cheese".to_string()]);
-        l0000.insert("name".to_string(), vec!["home".to_string()]);
-        locations.library.insert("0000".to_string(), l0000);
-
-        let mut l0001 = HashMap::new();
-        l0001.insert("description".to_string(), vec!["An old and derepit apartment building. Inside it can be navigated by a rather narrow hallway that is barely lit. People seemed to use the hallway as extra storage, even though there was no room for it.".to_string()]);
-        l0001.insert("nearby".to_string(), vec!["0000".to_string(), "0002".to_string()]);
-        l0001.insert("objects".to_string(), vec!["wooden box".to_string()]);
-        l0001.insert("name".to_string(), vec!["apartment building".to_string()]);
-        locations.library.insert("0001".to_string(), l0001);
-
-        let mut l0002 = HashMap::new();
-        l0002.insert("description".to_string(), vec!["One of the most poor districts in Anshanli; however it is still more safe than the pleasure district due to the volunteer work done by temple priests and nuns.".to_string()]);
-        l0002.insert("nearby".to_string(), vec!["0001".to_string(), "0003".to_string()]);
-        l0002.insert("objects".to_string(), vec![]);
-        l0002.insert("name".to_string(), vec!["dubari district".to_string()]);
-        locations.library.insert("0002".to_string(), l0002);
-
-        let mut l0003 = HashMap::new();
-        l0003.insert("description".to_string(), vec!["A bustling market filled with all sorts of people. The Dubari market in particular seemed to attract a colorful crowd, for better or worse. Among the delicious scents of food, one can catch a whiff of rotten fish, and something that reminds one of a dog's asshole.".to_string()]);
-        l0003.insert("nearby".to_string(), vec!["0002".to_string()]);
-        l0003.insert("objects".to_string(), vec!["food seller".to_string(), "apothecary".to_string(), "shady merchant".to_string()]);
-        l0003.insert("name".to_string(), vec!["dubari market".to_string()]);
-        locations.library.insert("0003".to_string(), l0003);
+        let _list = vec![
+            //vec!["id", "name", "description", "nearby", "objects"],
+            vec!["0000", "home", "A run down and kind of dirty single room apartment. There is a single Table in the center of the room with two chairs, a stove for cooking, and a few cupboards and countertops. There is not much else in this room besides a dresser for clothes and moldy cheese sitting on the table.", "0001", "modly cheese", ""],
+            vec!["0001", "apartment building", "An old and derepit apartment building. Inside it can be navigated by a rather narrow hallway that is barely lit. People seemed to use the hallway as extra storage, even though there was no room for it.", "0000;0002", "wood crate", ""],
+            vec!["0002", "dubari district", "One of the most poor districts in Anshanli; however it is still more safe than the pleasure district due to the volunteer work done by temple priests and nuns.", "0001;0003;0004", "", "dice player;suspicious stranger"],
+            vec!["0003", "dubari market", "A bustling market filled with all sorts of people. The Dubari market in particular seemes to attract a colorful crowd; for better or worse. Among the delicious scents of food, one can catch a whiff of rotten fish, and something that reminds one of a dead dog's asshole.", "0002", "", "food seller;apothecary;shady merchant"],
+            vec!["0004", "central anshanli", "The clean and prosperous center of the city. Home to many of the cities buisnesses and it's ornate temple to the Divine Mora. Patrolled often by the city guard, this is not a place where the wealthy feel unsafe.", "0002;0005;0006", "", ""],
+            vec!["0005", "anshanli guild", "A large, upscale multi-story building. The spacious room inside the imposing front doors also functions as a tavern. Often a lively place, filled with a tough looking crowd. A board hosting jobs for the guild members sits on the far wall.", "0004", "", "barkeep;job board;guild official"],
+            vec!["0006", "temple of mora", "Just as impressive on the inside as the outside. Ornate carvings cover the outisde of the building. Some are beautiful and conforting, while others show scenes of distruction and chaos. On the candle-lit inside, a vaulted ceiling is painted with intricate patterns. A few priests and nuns can be seen doing various duities.", "0004", "shrine", "head priest;head nun"],
+        ];
+        for i in _list {
+            let id = &i[0];
+            let name = &i[1];
+            let description = &i[2];
+            let nearby: Vec<String> = i[3].split(';')
+            .map(String::from)
+            .collect();
+            let items: Vec<String> = i[4].split(';')
+            .map(String::from)
+            .collect();
+            let npc: Vec<String> = i[5].split(';')
+            .map(String::from)
+            .collect();
+            let mut place = HashMap::new();
+            place.insert("name".to_string(), vec![name.to_string()]);
+            place.insert("description".to_string(), vec![description.to_string()]);
+            place.insert("nearby".to_string(), nearby);
+            place.insert("items".to_string(), items);
+            place.insert("npc".to_string(), npc);
+            locations.library.insert(id.to_string(), place);
+        }
         locations
     }
 }
